eb04bf329890d88d9308d1c1b1d1867102b987bd
Artisan <william.kwan@artisanconsultin.nz>
2024-06-30 16:12:06 +1000

    GET routes added

--- server/app.py
+++ server/app.py
@@ -2,6 +2,7 @@
 
 from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
+from sqlalchemy import desc, asc
 
 from models import db, Bakery, BakedGood
 
@@ -16,23 +17,29 @@ db.init_app(app)
 
 @app.route('/')
 def index():
-    return '<h1>Bakery GET API</h1>'
+    return '<h1>My Bakery GET API</h1>'
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    # return 'get all bakeries'
+    return jsonify([b.to_dict() for b in Bakery.query.all()])
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    # return all goods by bekery id
+    return jsonify(Bakery.query.get(id).to_dict())
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    # return 'get all baked goods by price' 
+    # sorts the baked goods by price in descending order
+    return jsonify([bg.to_dict() for bg in BakedGood.query.order_by(BakedGood.price.desc()).all()])
+   
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    # return 'get the most expensive baked good'
+    return jsonify(BakedGood.query.order_by(BakedGood.price.desc()).first().to_dict())
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
